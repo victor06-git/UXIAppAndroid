7cbb7ef2b3af5616e6af3a43b4755f8fa4b8303f
victor06-git <asensiovector@gmail.com>
2026-01-28 19:04:02 +0100

    Initial commit

---
+++ app/src/main/java/com/vasensio/uxiappandroid/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.vasensio.uxiappandroid.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
